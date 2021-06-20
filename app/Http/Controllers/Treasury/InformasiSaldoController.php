4c528df382f6644423e6b9c813394d2667c17f8e
erkalodon <andhika.ragilkesuma@gmail.com>
2021-06-20 20:00:11 +0700

    Add new modul and controller

---
+++ app/Http/Controllers/Treasury/InformasiSaldoController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Treasury;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class InformasiSaldoController extends Controller
+{
+    //
+}
