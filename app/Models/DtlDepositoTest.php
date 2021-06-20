0fb048059e1416de23b6ff24e857af4aac0fa642
erkalodon <andhika.ragilkesuma@gmail.com>
2021-06-21 00:03:57 +0700

    Add model

---
+++ app/Models/DtlDepositoTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DtlDepositoTest extends Model
+{
+    use HasFactory;
+}
