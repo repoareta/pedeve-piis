8d3536f1b0446268b5cd6ac5c5973a718bba7480
andhikaerka <andhika.ragilkesuma@gmail.com>
2021-07-22 16:32:22 +0700

    Add search form jurnal umum

--- resources/views/modul-kontroler/jurnal-umum/index.blade.php
+++ resources/views/modul-kontroler/jurnal-umum/index.blade.php
@@ -37,6 +37,41 @@
         </div>
     </div>
     <div class="card-body">
+
+        <div class="col-12">
+			<form class="kt-form" id="search-form" method="POST">
+				<div class="form-group row">
+					<label for="" class="col-form-label">BULAN</label>
+					<div class="col-3">
+						<select class="form-control select2" name="bulan" id="bulan">
+							<option value="" >-- Pilih --</option>
+							<option value="01" <?php if($bulan  == '01' ) echo 'selected' ; ?>>Januari</option>
+							<option value="02" <?php if($bulan  == '02' ) echo 'selected' ; ?>>Februari</option>
+							<option value="03" <?php if($bulan  == '03' ) echo 'selected' ; ?>>Maret</option>
+							<option value="04" <?php if($bulan  == '04' ) echo 'selected' ; ?>>April</option>
+							<option value="05" <?php if($bulan  == '05' ) echo 'selected' ; ?>>Mei</option>
+							<option value="06" <?php if($bulan  == '06' ) echo 'selected' ; ?>>Juni</option>
+							<option value="07" <?php if($bulan  == '07' ) echo 'selected' ; ?>>Juli</option>
+							<option value="08" <?php if($bulan  == '08' ) echo 'selected' ; ?>>Agustus</option>
+							<option value="09" <?php if($bulan  == '09' ) echo 'selected' ; ?>>September</option>
+							<option value="10" <?php if($bulan  == '10' ) echo 'selected' ; ?>>Oktober</option>
+							<option value="11" <?php if($bulan  == '11' ) echo 'selected' ; ?>>November</option>
+							<option value="12" <?php if($bulan  == '12' ) echo 'selected' ; ?>>Desember</option>
+						</select>
+					</div>
+
+                    <label for="" class="col-form-label">TAHUN</label>
+					<div class="col-2">
+						<input class="form-control" type="text" name="tahun" id="tahun" value="{{ date('Y') }}">
+					</div>
+
+					<div class="col-2">
+						<button type="submit" class="btn btn-primary"><i class="fa fa-search" aria-hidden="true"></i> Cari</button>
+					</div>
+				</div>
+			</form>
+		</div>
+        
         <div class="row">
             <div class="col-xl-12">
                 <table class="table table-bordered" id="kt_table" width="100%">
@@ -97,10 +132,16 @@
                 {data: 'action', name: 'action', class: 'text-center'},
             ]
         });
+
         $('#search-form').on('submit', function(e) {
             t.draw();
             e.preventDefault();
         });
+
+        $('.select2').select2().on('change', function() {
+            $(this).valid();
+        });
+        
         $('#kt_table tbody').on( 'click', 'tr', function (event) {
             if ( $(this).hasClass('selected') ) {
                 $(this).removeClass('selected');
