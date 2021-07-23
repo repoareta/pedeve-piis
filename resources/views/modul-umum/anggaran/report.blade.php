5cbb937693326acfb21621bc7752edf2186b665b
andhikaerka <andhika.ragilkesuma@gmail.com>
2021-07-23 10:36:28 +0700

    anggaran report form

---
+++ resources/views/modul-umum/anggaran/report.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('breadcrumbs')
+    {{ Breadcrumbs::render('set-user') }}
+@endsection
+
+@section('content')
+
+<div class="card card-custom card-sticky" id="kt_page_sticky_card">
+    <div class="card-header justify-content-right">
+        <div class="card-title">
+            <span class="card-icon">
+                <i class="flaticon2-line-chart text-primary"></i>
+            </span>
+            <h3 class="card-label">
+                Anggaran Report
+            </h3>
+        </div>
+    </div>
+    <div class="card-body">
+		<div class="col-12">
+			<form class="kt-form kt-form--label-right" action="{{ route('modul_umum.anggaran.report.export') }}" method="post">
+                @csrf
+                <div class="form-group row">
+                    <label for="mulai-input" class="col-2 col-form-label">Tahun</label>
+                    <div class="col-8">
+                        <input type="text" class="form-control" name="tahun" autocomplete="off" />
+                    </div>
+                </div>
+    
+                <div class="kt-form__actions">
+                    <div class="row">
+                        <div class="col-2"></div>
+                        <div class="col-10">
+                            <a  href="{{ url()->previous() }}" class="btn btn-warning"><i class="fa fa-reply" aria-hidden="true"></i> Batal</a>
+                            <button type="submit" class="btn btn-danger"><i class="fa fa-file-pdf" aria-hidden="true"></i> Export .PDF</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+		</div>
+    </div>
+</div>
+
+@endsection
