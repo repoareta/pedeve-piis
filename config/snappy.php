ccf467982d94c2b79caa5f88351edd9d7a523cdd
Benny Rahmat <akunbeben@gmail.com>
2021-07-29 10:44:46 +0700

    change load wkhtmltopdf from direct path to ENV 'WKHTML_PDF_BINARY'

--- config/snappy.php
+++ config/snappy.php
@@ -35,7 +35,7 @@ return [
     
     'pdf' => [
         'enabled' => true,
-        'binary'  => env('WKHTML_PDF_BINARY', '/usr/local/bin/wkhtmltopdf'),
+        'binary'  => env('WKHTML_PDF_BINARY', 'vendor\wemersonjanuario\wkhtmltopdf-windows\bin\64bit\wkhtmltopdf'),
         // 'binary'  => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
         // 'binary'  => base_path('vendor\wemersonjanuario\wkhtmltopdf-windows\bin\64bit\wkhtmltopdf'),
         'timeout' => 3600,
