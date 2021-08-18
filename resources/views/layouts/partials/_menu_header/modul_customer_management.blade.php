49b7d94decdd6d0e79de2e3c7a01f4a09cf7d9ff
andhikaerka <andhika.ragilkesuma@gmail.com>
2021-08-18 08:17:17 +0700

    Delete unused menu

--- resources/views/layouts/partials/_menu_header/modul_customer_management.blade.php
+++ resources/views/layouts/partials/_menu_header/modul_customer_management.blade.php
@@ -21,14 +21,14 @@
                     <span class="menu-text">Perusahaan Afiliasi</span>
                 </a>
             </li>
-            <li class="menu-item {{ Route::is('modul_cm.monitoring_kinerja.*') ? 'menu-item-active' : '' }}" aria-haspopup="true">
+            {{-- <li class="menu-item {{ Route::is('modul_cm.monitoring_kinerja.*') ? 'menu-item-active' : '' }}" aria-haspopup="true">
                 <a href="{{ route('modul_cm.monitoring_kinerja.index') }}" class="menu-link">
                     <i class="menu-bullet menu-bullet-line">
                         <span></span>
                     </i>
                     <span class="menu-text">Monitoring Kinerja</span>
                 </a>
-            </li>
+            </li> --}}
             <li class="menu-item {{ Route::is('modul_cm.rkap_realisasi.*') ? 'menu-item-active' : '' }}" aria-haspopup="true">
                 <a href="{{ route('modul_cm.rkap_realisasi.index') }}" class="menu-link">
                     <i class="menu-bullet menu-bullet-line">
@@ -37,14 +37,14 @@
                     <span class="menu-text">RKAP & Realisasi</span>
                 </a>
             </li>
-            <li class="menu-item {{ Route::is('modul_cm.pencapaian_kinerja.*') ? 'menu-item-active' : '' }}" aria-haspopup="true">
+            {{-- <li class="menu-item {{ Route::is('modul_cm.pencapaian_kinerja.*') ? 'menu-item-active' : '' }}" aria-haspopup="true">
                 <a href="{{ route('modul_cm.pencapaian_kinerja.index') }}" class="menu-link">
                     <i class="menu-bullet menu-bullet-line">
                         <span></span>
                     </i>
                     <span class="menu-text">Pencapaian Kinerja</span>
                 </a>
-            </li>
+            </li> --}}
         </ul>
     </div>
 </li>
