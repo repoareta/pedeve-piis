d09e07206c08cb2dd37ba14b4f1302abb988c5b1
Benny Rahmat <akunbeben@gmail.com>
2021-08-18 16:21:01 +0700

    refactor: required changed to nullable - perusahaan-afiliasi

--- app/Http/Requests/PerusahaanAfiliasiStore.php
+++ app/Http/Requests/PerusahaanAfiliasiStore.php
@@ -26,9 +26,9 @@ class PerusahaanAfiliasiStore extends FormRequest
         return [
             'nama_perusahaan' => 'required|unique:App\Models\PerusahaanAfiliasi,nama',
             'alamat' => 'required',
-            'no_telepon' => 'nullable|string|digits_between:6,15',
+            'no_telepon' => 'string|digits_between:6,15',
             'bidang_usaha' => 'required',
-            'npwp' => 'nullable',
+            'npwp' => 'string',
             'modal_dasar' => 'required|numeric|min:0',
             'modal_disetor' => 'required|numeric|min:0',
             'jumlah_lembar_saham' => 'required|integer|min:0',
