ffa8d9948df57ca5aaa7f34a2cda25df21e1ce72
Benny Rahmat <akunbeben@gmail.com>
2021-08-19 18:22:46 +0700

    feat: apply form request - penempatan-deposito

---
+++ app/Http/Requests/PenempatanDepositoStoreRequest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PenempatanDepositoStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'nodok' => 'required|string',
+            'namabank' => 'required',
+            'nominal' => 'required|numeric',
+            'tanggal' => 'required|date_format:d-m-Y',
+            'tanggal2' => 'required|date_format:d-m-Y',
+            'noseri' => 'required|string',
+            'lineno' => 'required',
+            'asal' => 'required',
+            'kdbank' => 'required',
+            'tahunbunga' => 'required',
+            'perpanjangan' => 'required',
+            'keterangan' => 'required',
+            'kurs' => 'required',
+        ];
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'nodok' => 'Nomor Dokumen',
+            'namabank' => 'Nama Bank',
+            'nominal' => 'Nominal',
+            'tanggal' => 'Tanggal',
+            'tanggal2' => 'Jatuh Tempo',
+            'tahunbunga' => 'Bunga',
+            'noseri' => 'Nomor Seri',
+        ];
+    }
+}
