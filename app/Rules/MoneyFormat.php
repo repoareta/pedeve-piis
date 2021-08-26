579437ee797c17429e3c9331f6e62a9428c01338
Benny Rahmat <akunbeben@gmail.com>
2021-08-27 02:13:29 +0700

    feat: added new Validation Rule for numeric number

---
+++ app/Rules/MoneyFormat.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class MoneyFormat implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $regex = '/^\d+(,\d{3})*(\.\d+)?$/';
+
+        $validate = preg_match($regex, $value);
+
+        if ($validate == 1) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'Format :attribute tidak valid.';
+    }
+}
