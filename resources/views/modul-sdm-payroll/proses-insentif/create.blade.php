0b4780855ef01e27d59524a70d6b72696e083131
Benny Rahmat <46495960+akunbeben@users.noreply.github.com>
2021-10-08 14:41:22 +0700

    feat: update proses insentif create event-listener (#129)

--- resources/views/modul-sdm-payroll/proses-insentif/create.blade.php
+++ resources/views/modul-sdm-payroll/proses-insentif/create.blade.php
@@ -31,34 +31,34 @@
                     </div>
                     <input class="form-control" type="hidden" name="userid" value="{{ Auth::user()->userid }}">
                     <div class="form-group row">
-                        <label for="dari-input" class="col-2 col-form-label">Status Pekerja<span class="text-danger">*</span></label>
+                        <label for="dari-input" class="col-2 col-form-label">Status Pekerja <span class="text-danger">*</span></label>
                         <div class="col-10">
                             <select name="prosesupah" id="select-debetdari" class="form-control select2" style="width: 100% !important;">
                                 <option value="A">Semua</option>
                                 <option value="C">Pekerja Tetap</option>
                                 <option value="K">Kontrak</option>
-                            </select>								
+                            </select>
                         </div>
                     </div>
                     <div class="form-group row">
-                        <?php 
+                        <?php
                             $tgl = date_create(now());
-                            $bulantahun = date_format($tgl, 'm/Y'); 
-                            $tahun = date_format($tgl, 'Y'); 
+                            $bulantahun = date_format($tgl, 'm/Y');
+                            $tahun = date_format($tgl, 'Y');
                         ?>
-                        <label for="nopek-input" class="col-2 col-form-label">Bulan/Tahun<span class="text-danger">*</span></label>
+                        <label for="nopek-input" class="col-2 col-form-label">Bulan/Tahun <span class="text-danger">*</span></label>
                         <div class="col-10">
                             <input class="form-control" type="text" name="tanggal" value="{{ $bulantahun }}" id="tanggal" required autocomplete="off"">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="nopek-input" class="col-2 col-form-label">Tahun Insentif<span class="text-danger">*</span></label>
+                        <label for="nopek-input" class="col-2 col-form-label">Tahun Insentif <span class="text-danger">*</span></label>
                         <div class="col-10">
                             <input class="form-control tahun" type="text" name="tahun" value="{{ $tahun }}" autocomplete="off" required">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="nopek-input" class="col-2 col-form-label">Upah Tetap/All In<span class="text-danger">*</span></label>
+                        <label for="nopek-input" class="col-2 col-form-label">Upah Tetap/All In <span class="text-danger">*</span></label>
                         <div class="col-10">
                             <input class="form-control" type="text" name="upah" value="" autocomplete="off" required">
                         </div>
@@ -69,7 +69,7 @@
                             <input class="form-control" type="text" name="keterangan" value="" size="30" maxlength="30"  autocomplete="off">
                         </div>
                     </div>
-    
+
                     <div class="form-group row">
                         <label for="" class="col-2 col-form-label"></label>
                         <div class="col-8">
@@ -107,7 +107,7 @@
 	$(document).ready(function () {
         // minimum setup
         $('#tanggal').datepicker({
-            startView: "months", 
+            startView: "months",
             minViewMode: "months",
             orientation: "bottom left",
             autoclose: true,
