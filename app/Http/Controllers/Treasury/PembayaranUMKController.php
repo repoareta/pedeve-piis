cf4ae1fecaac1d7e2c696f921fe92a12bc845f54
Benny Rahmat <46495960+akunbeben@users.noreply.github.com>
2021-11-25 14:24:07 +0700

    refactor: change some raw query into eloquent & qbuilder (#151)

--- app/Http/Controllers/Treasury/PembayaranUMKController.php
+++ app/Http/Controllers/Treasury/PembayaranUMKController.php
@@ -12,6 +12,7 @@ use App\Models\Lokasi;
 use App\Models\SaldoStore;
 use App\Models\SdmKDBag;
 use App\Models\Umk;
+use App\Services\TimeTransactionService;
 use DomPDF;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -19,20 +20,22 @@ use RealRashid\SweetAlert\Facades\Alert;
 
 class PembayaranUMKController extends Controller
 {
+    protected $timeTrans;
+
+    public function __construct(TimeTransactionService $timeTrans)
+    {
+        $this->timeTrans = $timeTrans;
+    }
+
     public function index()
     {
-        $data_tahunbulan = DB::select("SELECT max(thnbln) as bulan_buku from timetrans where status='1' and length(thnbln)='6'");
-        if (!empty($data_tahunbulan)) {
-            foreach ($data_tahunbulan as $data_bul) {
-                $tahun = substr($data_bul->bulan_buku, 0, -2);
-                $bulan = substr($data_bul->bulan_buku, 4);
-            }
-        } else {
-            $bulan = date('m');
-            $tahun = date('Y');
-        }
+        $tahun = $this->timeTrans->getCurrentYear();
+        $bulan = $this->timeTrans->getCurrentMonth();
 
-        return view('modul-treasury.pembayaran-umk.index', compact('tahun', 'bulan'));
+        return view('modul-treasury.pembayaran-umk.index', compact(
+            'tahun',
+            'bulan',
+        ));
     }
 
     public function indexJson(Request $request)
