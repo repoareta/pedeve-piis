f57ecba6789ac805a7aa345a8dba24c2e322dad3
Benny Rahmat <46495960+akunbeben@users.noreply.github.com>
2021-12-30 13:21:34 +0800

    fix: perjalanan dinas (#183)

--- resources/views/modul-umum/perjalanan-dinas/index.blade.php
+++ resources/views/modul-umum/perjalanan-dinas/index.blade.php
@@ -96,7 +96,7 @@
 	<div class="modal-dialog modal-lg" role="document">
 		<div class="modal-content">
 			<div class="modal-header">
-				<h5 class="modal-title">Cetak Data</h5>
+				<h5 class="modal-title">Cetak Surat Keterangan Perjalanan Dinas</h5>
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
 				</button>
